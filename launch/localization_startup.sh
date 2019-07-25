51a6e844edbb626516c9b692ec5543311a0c78aa
larsenkw <kylew.larsen@gmail.com>
2019-07-25 10:42:45 -0600

    updated grasping launch

--- launch/localization_startup.sh
+++ launch/localization_startup.sh
@@ -4,7 +4,7 @@
 # ~/cartographer_latest/src/cartographer_ros/cartographer_ros/launch/my_robot_2d_localization.launch
 # ~/cartographer_latest/src/cartographer_ros/cartographer_ros/configuration_files/realtime_2d.lua
 source ~/cartographer_latest/devel_isolated/setup.bash
-roslaunch cartographer_ros my_robot_2d_localization_cut.launch &
+roslaunch cartographer_ros my_robot_2d_localization_cut.launch > /dev/null &
 
 # Store background PID, which is the cartographer launch above
 bg_pid=$!
@@ -17,7 +17,7 @@ sleep 2
 # before curve starting position to new origin: translation={-2, -8, 0},rotation={0.0, 0.0, 1.57079632679}
 
 rosservice call /finish_trajectory "trajectory_id: 1"
-rosrun cartographer_ros cartographer_start_trajectory -configuration_directory '/home/csm/cartographer_latest/install_isolated/share/cartographer_ros/configuration_files' -configuration_basename 'realtime_2d.lua' -initial_pose '{to_trajectory_id = 0,relative_pose = {translation={0, 0, 0},rotation = {0.0, 0.0, 0}},timestamp = 0}' &
+rosrun cartographer_ros cartographer_start_trajectory -configuration_directory '/home/csm/cartographer_latest/install_isolated/share/cartographer_ros/configuration_files' -configuration_basename 'realtime_2d.lua' -initial_pose '{to_trajectory_id = 0,relative_pose = {translation={4, 4, 0},rotation = {0.0, 0.0, 0}},timestamp = 0}' > /dev/null &
 #{translation={-1.9, 0, 0},rotation = {0.0, 0.0, 3.14159265359}}
 
 # Store background PID for rosrun command
