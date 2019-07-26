2c7418fc2fdaf98c6b59f5945925f163ff4c000e
larsenkw <kylew.larsen@gmail.com>
2019-07-25 22:43:52 -0600

    updated obstacle avoidance launch files

--- launch/localization_startup.sh
+++ launch/localization_startup.sh
@@ -17,7 +17,7 @@ sleep 2
 # before curve starting position to new origin: translation={-2, -8, 0},rotation={0.0, 0.0, 1.57079632679}
 
 rosservice call /finish_trajectory "trajectory_id: 1"
-rosrun cartographer_ros cartographer_start_trajectory -configuration_directory '/home/csm/cartographer_latest/install_isolated/share/cartographer_ros/configuration_files' -configuration_basename 'realtime_2d.lua' -initial_pose '{to_trajectory_id = 0,relative_pose = {translation={4, 4, 0},rotation = {0.0, 0.0, 0}},timestamp = 0}' > /dev/null &
+rosrun cartographer_ros cartographer_start_trajectory -configuration_directory '/home/csm/cartographer_latest/install_isolated/share/cartographer_ros/configuration_files' -configuration_basename 'realtime_2d.lua' -initial_pose '{to_trajectory_id = 0,relative_pose = {translation={0, 0, 0},rotation = {0.0, 0.0, -1.571}},timestamp = 0}' > /dev/null &
 #{translation={-1.9, 0, 0},rotation = {0.0, 0.0, 3.14159265359}}
 
 # Store background PID for rosrun command
