c46e2a37b9b9a75edc190163678148c7a24c0d02
larsenkw <kylew.larsen@gmail.com>
2019-08-07 12:48:49 -0600

    added script for updating git repositories

---
+++ launch/update_git.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Pulls all of the forklift_ws github repositories
+
+echo "Updaing github repositories"
+
+cd $HOME/forklift_ws/src/forklift_bringup
+git pull
+
+cd ../grasping
+git pull
+
+cd ../mapping
+git pull
+
+cd ../motion_testing
+git pull
+
+cd ../ouster_example
+git pull
+
+cd ../robot_localization
+git pull
+
+cd ../robust_navigation
+git pull
+
+cd ../sensors
+git pull
+
+cd ../setup_tf
+git pull
+
+echo "Finished updating repositories"
