a7cac0b52df26ce07a60de3b933379f107a5fe8a
larsenkw <kylew.larsen@gmail.com>
2019-08-06 20:10:00 -0600

    added standalone launch file for cartographer

--- launch/localization_startup.sh
+++ launch/localization_startup.sh
@@ -16,13 +16,16 @@ trap "kill -2 $bg_pid" 2
 sleep 2
 
 # before curve starting position to new origin: translation={-2, -8, 0},rotation={0.0, 0.0, 1.57079632679}
+PI=3.141592654
+PI_DIV_2=`echo "scale=9; $PI/2" | bc` # note the 'angled' apostrophe ` not '
+STARTING_POINT="translation={-1.9, 0, 0},rotation={0.0, 0.0, 3.14159265359}"
 
 rosservice call /finish_trajectory "trajectory_id: 1"
 # Starting point for demo
 #rosrun cartographer_ros cartographer_start_trajectory -configuration_directory '/home/csm/cartographer_latest/install_isolated/share/cartographer_ros/configuration_files' -configuration_basename 'realtime_2d.lua' -initial_pose '{to_trajectory_id = 0,relative_pose = {translation={4, 16, 0},rotation = {0.0, 0.0, 1.571}},timestamp = 0}' > /dev/null &
 # Test starting point
-rosrun cartographer_ros cartographer_start_trajectory -configuration_directory '/home/csm/cartographer_latest/install_isolated/share/cartographer_ros/configuration_files' -configuration_basename 'realtime_2d_turtlebot.lua' -initial_pose '{to_trajectory_id = 0,relative_pose = {translation={3.5, 14, 0},rotation = {0.0, 0.0, -1.5}}, timestamp = 0}' &
-#{translation={-1.9, 0, 0},rotation = {0.0, 0.0, 3.14159265359}}
+rosrun cartographer_ros cartographer_start_trajectory -configuration_directory "/home/csm/cartographer_latest/install_isolated/share/cartographer_ros/configuration_files" -configuration_basename "realtime_2d_turtlebot.lua" -initial_pose "{to_trajectory_id = 0,relative_pose = {$STARTING_POINT}, timestamp = 0}" &
+# {translation={-1.9, 0, 0},rotation = {0.0, 0.0, 3.14159265359}}
 
 # Store background PID for rosrun command
 bg_pid2=$!
