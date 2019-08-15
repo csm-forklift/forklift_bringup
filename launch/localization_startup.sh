51a9e91f557dfbc7d753fed578342e9fd5e9ae00
larsenkw <kylew.larsen@gmail.com>
2019-08-14 20:25:46 -0600

    added grasping starting point

--- launch/localization_startup.sh
+++ launch/localization_startup.sh
@@ -23,7 +23,10 @@ NEG_PI_DIV_2=`echo "scale=9; $NEG_PI/2" | bc`
 #===== Official Starting Point - DO NOT CHANGE! =====#
 STARTING_POINT="translation={3.3, 14.5, 0},rotation={0.0, 0.0, `echo "scale=9; $NEG_PI_DIV_2+0.1" | bc`}"
 #====================================================#
+#----- Starting point for maneuver
 #STARTING_POINT="translation={-4.5, 2.0, 0},rotation={0.0, 0.0, `echo "scale=9; (-3.0/4.0)*${PI}+0.15" | bc`}"
+#----- Starting point for grasping
+#STARTING_POINT="translation={0.0, 5.0, 0.0},rotation={0.0, 0.0, $NEG_PI}"
 
 rosservice call /finish_trajectory "trajectory_id: 1"
 # Starting point for demo
